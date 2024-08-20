2e8ddc3056485e0dce41223b114b1022ff7a7c72
FakeGoblinn <128422010+FakeGoblinn@users.noreply.github.com>
2024-08-20 17:26:38 -0300

    Fase 1
    Fundo adicionado, troca de TMP por Text comum para uso de fonte especifica

--- TCPIV/Assets/Scripts/Timer.cs
+++ TCPIV/Assets/Scripts/Timer.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private TMP_Text txtTime;
+    [SerializeField] private Text txtTime;
     [SerializeField] private float timeValue;
 
 
